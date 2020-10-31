fcd6af736f9e3ac571a98d4759cbdf21a2ee0cff
Josh8903 <thorngren87@gmail.com>
2020-10-30 20:38:11 -0500

    index.handlebars fix - getting html output now

--- config/orm.js
+++ config/orm.js
@@ -4,15 +4,17 @@ var connection = require("./connection.js");
 // The ? signs are for swapping out other values
 
 var orm = {
-    selectAll: function(cb) {
-        var queryString = "SELECT * FROM burgers";
+    selectAll: function(asdf, cb) {
+        var queryString = "SELECT * FROM burgers;";
         connection.query(queryString, function(err, result) {
-            if (err) throw err;
+            if (err) {
+                throw err;
+            }
             console.log(result);
             cb(result);
         });
     },
-    insertOne: function(burgerName, devoured) {
+    insertOne: function(burgerName, devoured, cb) {
         var queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (?, ?)";
         console.log(queryString);
         connection.query(queryString, burgerName, devoured, function(err, result) {
@@ -21,7 +23,7 @@ var orm = {
             cb(result);
         });
     },
-    updateOne: function(devoured, condition) {
+    updateOne: function(devoured, condition, cb) {
         var queryString = "UPDATE burgers SET (devoured) VALUES (?) WHERE ";
         quertString += condition;
         connection.query(queryString, [devoured], function(err, result) {
